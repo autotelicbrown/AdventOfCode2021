b35a03bf05c03e44ac132266bc5c37b62598ccb3
Autotelic <autotelicbrown@gmail.com>
2021-12-07 20:09:52 -0200

    Added solution for day 01 part 2

---
+++ Day01/bopis2.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <queue>
+
+int main(int argc, char *argv[])
+{
+  const int SUM_RANGE = 3;
+  std::queue<int> Q;
+  int count = 0;
+  int val;
+
+  for (int i = 0; i < SUM_RANGE; ++i) {
+    scanf("%d", &val);
+    Q.push(val);
+  }
+
+  while (scanf("%d", &val) != EOF) {
+    // Shared elements can be ignored, only new and oldest values matter
+    if (val > Q.front())
+      ++count;
+    Q.pop();
+    Q.push(val);
+  }
+
+  printf("Answer: %d\n", count);
+
+  return 0;
+}
+
